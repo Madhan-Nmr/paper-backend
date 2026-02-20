93545efef18829219fb1a327c701efb30da8ea2a
Madhan-Nmr <madhanenterprises44@gmail.com>
2026-02-20 23:12:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/getPrice", async (req, res) => {
   });
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
